569ede3f5b97fea346d3f7d606ff9ee4e44dc91d
Sxoony <zamirkruger92@gmail.com>
2025-11-11 13:34:30 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            string filepath = "Students.xml";
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            
+             XmlDocument doc = new XmlDocument();
+            using (XmlWriter writer = XmlWriter.Create(filepath, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("School");
+               
+
+                
+                for (int i = 0; i < 5; i++)
+                {
+                    writer.WriteStartElement("Student");
+                    writer.WriteAttributeString("StudentID",Convert.ToString(i));
+                    Console.WriteLine("Enter name:");
+                    writer.WriteElementString("Name", Console.ReadLine());
+                    Console.WriteLine("Enter age:");
+                    writer.WriteElementString("Age", Console.ReadLine());
+                    Console.WriteLine("Enter Grade:");
+                    writer.WriteElementString("Grade", Console.ReadLine());
+                    Console.WriteLine("Enter Gender:");
+                    writer.WriteElementString("Gender", Console.ReadLine());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+               
+            }
+            Console.WriteLine("XML File successfully created.");
+            Process.Start("notepad.exe", filepath);
+        }
+
+        
+    }
+}
---
+++ README.txt
@@ -0,0 +1,38 @@
+# Project Title
+
+
+Short one-line summary of what the project does.
+
+
+## Description
+Write 2–4 sentences explaining what the project does and why.
+
+
+## Features
+- Feature A
+- Feature B
+
+
+## Technologies
+- Language / Framework: e.g. Python 3.11, Node 18, .NET 8
+- Libraries: list any major ones
+
+
+## How to run (local)
+1. Clone: `git clone https://github.com/YOURUSERNAME/REPO-NAME.git`
+2. Navigate: `cd REPO-NAME`
+3. Setup:
+- `pip install -r requirements.txt` (Python)
+- `npm install` (Node)
+- Open in Visual Studio (C#)
+4. Run:
+- `python main.py` / `node index.js`
+
+
+## Notes / TODO
+- Add feature X
+- Improve UI
+
+
+## Author
+Your Name
